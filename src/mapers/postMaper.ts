59b010673ee058988d57065b66bf97e04ae1cf2b
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-02-26 21:35:18 +0300

    fix.........+create tests

--- src/mapers/postMaper.ts
+++ src/mapers/postMaper.ts
@@ -2,15 +2,23 @@ import {WithId} from "mongodb";
 import {OutputPost, Post} from "../allTypes/postTypes";
 
 
-
-export const postMaper = (post:WithId<Post>):OutputPost => {
+export const postMaper = (post: WithId<Post>): OutputPost => {
     return {
-        id:post._id.toString(),
         title: post.title,
         shortDescription: post.shortDescription,
         content: post.content,
-        blogId:post.blogId,
-        blogName:post.blogName,
-        createdAt:post.createdAt
+        blogId: post.blogId,
+        blogName: post.blogName,
+        createdAt: post.createdAt,
+        id: post._id.toString(),
     }
+    /*    return {
+            id:post._id.toString(),
+            title: post.title,
+            shortDescription: post.shortDescription,
+            content: post.content,
+            blogId:post.blogId,
+            blogName:post.blogName,
+            createdAt:post.createdAt
+        }*/
 }

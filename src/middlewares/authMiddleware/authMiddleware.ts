9e13d3d4b09fc8b303585049f66136bf207b5fba
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-02-26 20:22:59 +0300

    fix.......+create tests

--- src/middlewares/authMiddleware/authMiddleware.ts
+++ src/middlewares/authMiddleware/authMiddleware.ts
@@ -9,31 +9,3 @@ export const authMiddleware = (req:Request,res:Response,next:NextFunction)=>{
     } else {res.sendStatus(STATUS_CODE.UNAUTHORIZED_401)}
 }
 
-
-
-
-
-
-
-
-/*
-import {NextFunction, Response, Request} from "express";
-
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const LOGIN = 'login'
-    const PASSWORD = 'qwerty'
-    const auth = req.headers['authorization']
-
-    if (!auth) {
-        res.sendStatus(401)
-        return
-    }
-    const [basic, token] = auth.split(' ')
-    const decodedToken = Buffer.from(token, 'base64').toString()
-    const [login, password] = decodedToken.split(':')
-    if (login === LOGIN && password === PASSWORD) {
-        next()
-    } else {
-        res.sendStatus(401)
-    }
-}*/
